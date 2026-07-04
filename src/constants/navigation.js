bcbe4296e4b00fd2087e2a7cc596a89f6247ab53
Anil <anil@Anils-MacBook-Pro.local>
2026-07-04 10:31:00 +0530

    chore: create  project architecture and component structure

---
+++ src/constants/navigation.js
@@ -0,0 +1,8 @@
+export const NAVIGATION = [
+  { label: "Home", href: "/" },
+  { label: "Projects", href: "/projects" },
+  { label: "Services", href: "/services" },
+  { label: "About", href: "/about" },
+  { label: "Resume", href: "/resume" },
+  { label: "Contact", href: "/contact" },
+];
